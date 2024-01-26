51e16dad2779842ab979401de2b1bec8262aaf2c
NicoloCammarata <nicolo.camma@gmail.com>
2024-01-26 15:56:28 +0100

    props-aggiunte

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -11,63 +11,12 @@ export default {
             
         }
     },
-    methods:{
-        lenguage(lingua){
-            console.log('lingua' , lingua)
-            let queryString = ''
-            const flag = [
-                'ja',
-                'en',
-                'it',
-                'es'
-            ]
-            if(flag.includes(lingua)){
-                if (flag[0] == lingua) {
-                    queryString = 'https://flagicons.lipis.dev/flags/4x3/jp.svg'
-
-                    return queryString;
-                
-                
-                
-                }
-                else if(flag[1] == lingua){
-                    queryString = 'https://flagicons.lipis.dev/flags/4x3/gb.svg'
-
-                    return queryString;
-                
-
-                }
-                else{
-                    queryString = 'https://flagicons.lipis.dev/flags/4x3/' + lingua + '.svg'
-
-                    return queryString;
-                
-                
-                }
-
-            }
-            else{
-                queryString = 'https://flagicons.lipis.dev/flags/4x3/cy.svg'
-
-                return queryString;
-
-            }
-            
-            
-             
-        },
-        valueFilm(voto){
-            if(voto > 0.5){
-                classlist.add('buono')
-            }
-            
-        }
-        
-    },
     components: {
         appSingleProduct
 
-    }
+    },
+    
+    
 }
 </script>
 
@@ -75,76 +24,38 @@ export default {
     <main>
         <input v-model="store.searchText" type="text">
         <button @click="$emit('searchFilm')" >search</button>
-        <div>
-            <h2>Film</h2>
+        <h2>Film</h2>
             
             
-            <ul v-for="(film, i) in store.films">
-                <li>
-                    <img :src="'https://image.tmdb.org/t/p/w342' + film.poster_path" alt="film.title">
-                    
-                </li>
-                <li >
-                    {{ film.title }}
-                </li>
-                <li >
-                    {{ film.original_title }}
-                </li>
-                <li >
-                    {{ film.original_language }}
-                    
-                </li>
-                <li class="flag">
-                    <img  :src="lenguage(film.original_language)">
-                </li>
-                <li >
-                    {{ film.vote_average }}
-                    <i :class="{ 'buono': film.vote_average > 1.500 }" class="fa-regular fa-star" ></i>
-                    <i :class="{ 'buono': film.vote_average > 3.500 }" class="fa-regular fa-star"></i>
-                    <i :class="{ 'buono': film.vote_average > 5.500 }" class="fa-regular fa-star"></i>
-                    <i :class="{ 'buono': film.vote_average > 7.500 }" class="fa-regular fa-star"></i>
-                    <i :class="{ 'buono': film.vote_average > 9.500 }" class="fa-regular fa-star"></i>
-                </li>
+        <ul v-for="(film, i) in store.films">
+                          
 
-               <hr>
+            <appSingleProduct 
+            :title="film.title"
+            :ogTitle="film.original_title"
+            :lingua="film.original_language"
+            :vote="film.vote_average"
+            :poster="film.poster_path"
+            /> 
+                
 
 
-            </ul>
-        </div>
-        <div>
-            <h2>Serie</h2>
-            <ul v-for="(serie, i) in store.series">
-                <li>
-                    <img :src="'https://image.tmdb.org/t/p/w342' + serie.poster_path" alt="serie.title">
-                    
-                </li>
-                <li >
-                    {{ serie.name }}
-                </li>
-                <li >
-                    {{ serie.original_name }}
-                </li>
-                <li >
-                    {{ serie.original_language }}
-                    
-                </li>
-                <li class="flag">
-                    <img  :src="lenguage(serie.original_language)">
-                </li>
-                <li >
-                    {{ serie.vote_average }}
-                    <i :class="{ 'buono': serie.vote_average > 1.500 }" class="fa-regular fa-star" ></i>
-                    <i :class="{ 'buono': serie.vote_average > 3.500 }" class="fa-regular fa-star"></i>
-                    <i :class="{ 'buono': serie.vote_average > 5.500 }" class="fa-regular fa-star"></i>
-                    <i :class="{ 'buono': serie.vote_average > 7.500 }" class="fa-regular fa-star"></i>
-                    <i :class="{ 'buono': serie.vote_average > 9.500 }" class="fa-regular fa-star"></i>
-                </li>
-
-               <hr>
+        </ul>
+        <h2>Serie</h2>
+        <ul v-for="(serie, i) in store.series">
+            <appSingleProduct 
+            :title="serie.name"
+            :ogTitle="serie.original_name"
+            :lingua="serie.original_language"
+            :vote="serie.vote_average"
+            :poster="serie.poster_path"
+            />
+            
+                
 
 
-            </ul>
-        </div>
+        </ul>
+        
     </main>
     
   
--- src/components/appSingleProduct.vue
+++ src/components/appSingleProduct.vue
@@ -1,24 +1,124 @@
 <script >
+
 export default {
     data() {
         return {
+            
             
         }
             
     },
     props: {
-        series: Object
+        title: String,
+        ogTitle: String,
+        lingua: String,
+        vote: Number,
+        poster: String
+
 
     },
+    methods:{
+        lenguage(lingua){
+            console.log('lingua' , lingua)
+            let queryString = ''
+            const flag = [
+                'ja',
+                'en',
+                'it',
+                'es',
+                'de',
+                'ru',
+                'fr'
+            ]
+            if(flag.includes(lingua)){
+                if (flag[0] == lingua) {
+                    queryString = 'https://flagicons.lipis.dev/flags/4x3/jp.svg'
+
+                    return queryString;
+                
+                
+                
+                }
+                else if(flag[1] == lingua){
+                    queryString = 'https://flagicons.lipis.dev/flags/4x3/gb.svg'
+
+                    return queryString;
+                
+
+                }
+                else{
+                    queryString = 'https://flagicons.lipis.dev/flags/4x3/' + lingua + '.svg'
+
+                    return queryString;
+                
+                
+                }
+
+            }
+            else{
+                queryString = 'https://flagicons.lipis.dev/flags/4x3/cy.svg'
+
+                return queryString;
+
+            }
+            
+            
+             
+        },
+
+    },
+    
 }
 </script>
 
 <template>
-   
+
+  <div>
+    <li>
+        <img :src="'https://image.tmdb.org/t/p/w342' + poster" alt="film.title">
+                    
+    </li>
+    <li >
+        {{ title }}
+    </li>                
+                    
+    <li >
+        {{ ogTitle }}
+    </li>
+    <li >
+        {{ lingua }}
+                    
+    </li>
+    <li class="flag">
+        <img  :src="lenguage(lingua)">
+    </li>                
+    <li >
+        {{ vote }}
+        <i :class="{ 'buono': vote > 1.500 }" class="fa-regular fa-star" ></i>
+        <i :class="{ 'buono': vote > 3.500 }" class="fa-regular fa-star"></i>
+        <i :class="{ 'buono': vote > 5.500 }" class="fa-regular fa-star"></i>
+        <i :class="{ 'buono': vote > 7.500 }" class="fa-regular fa-star"></i>
+        <i :class="{ 'buono': vote > 9.500 }" class="fa-regular fa-star"></i>
+    </li>      
+    
+                         
+
+  </div>
+  <hr>      
+
   
 </template>
 
 <style lang="scss" scoped>
+.flag{
+    >img{
+        height: 50px;
+    }
+}
+
+.buono{
+    background-color: yellow;
+}
 
 
 </style>
